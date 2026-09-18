2d9356ede568630065bad8bd24b4b6c7cf299774
Chiara <chiaracasali3101@gmail.com>
2026-09-18 14:31:24 +0200

    gragle

---
+++ src/main/java/it/unibo/gridgames/persistence/ScoreManagerTest.java
@@ -0,0 +1,21 @@
+package main.java.it.unibo.gridgames.persistence;
+
+import it.unibo.gridgames.model.GameRecord;
+import it.unibo.gridgames.model.GameType;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class ScoreManagerTest {
+    
+}
