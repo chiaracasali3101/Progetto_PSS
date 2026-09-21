5c33438a226aea4c5088779f24ac95749fb60f5b
Chiara <chiaracasali3101@gmail.com>
2026-09-21 18:24:54 +0200

    ScoreManager.java

--- src/main/java/it/unibo/gridgames/persistence/ScoreManager.java
+++ src/main/java/it/unibo/gridgames/persistence/ScoreManager.java
@@ -1,6 +1,19 @@
-package main.java.it.unibo.gridgames.persistence;
+package it.unibo.gridgames.persistence;
+
+import it.unibo.gridgames.model.GameRecord;
+import it.unibo.gridgames.model.GameType;
+
+import java.io.IOException;
+import java.util.List;
 
 //interfaccia per la gestione dei punteggi dei giochi
-public class ScoreManager {
-    
+public interface ScoreManager {
+    //salva il punteggio di una partita appena finita nel file di persistenza
+    void saveScore(GameRecord record) throws IOException;
+
+    //riapre il file e converte tutti i record in oggetti GameRecord restituendo la lista
+    List<GameRecord> loadAllScores() throws IOException;
+
+    //estrae la classifica filtrando il gioco e limitando il numero di record visualizzati
+    List<GameRecord> getTopScores(GameType gameType, int limit);
 }
